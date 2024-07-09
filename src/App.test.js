3dd1ac1e3103e9a35c2c050512a97633c0ff554e
umerDev <umerraja@live.co.uk>
2024-07-09 20:21:53 +0100

    update readme.

--- src/App.test.js
+++ src/App.test.js
@@ -9,63 +9,10 @@ test("renders home view text", () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test("renders button for increment", () => {
+test("renders button for search", () => {
   //arrange
   render(<App />);
-  const button = screen.getByRole("button", { name: "Increment" });
+  const button = screen.getByRole("button", { name: "search" });
   //assert
   expect(button).toBeInTheDocument();
-});
-
-test("renders button for decrement", () => {
-  //arrange
-  render(<App />);
-  const button = screen.getByRole("button", { name: "Decrement" });
-  //assert
-  expect(button).toBeInTheDocument();
-});
-
-test("increase counter by 1", () => {
-  //arrange
-  render(<App />);
-  const button = screen.getByRole("button", { name: "Increment" });
-  //act
-  const buttonClick = fireEvent.click(button);
-  //assert
-  expect(screen.getByText(1)).toBeInTheDocument();
-  expect(buttonClick).toBeTruthy();
-});
-
-test("decrease counter by 1", () => {
-  //arrange
-  render(<App />);
-  const button = screen.getByRole("button", { name: "Decrement" });
-  //act
-  const buttonClick = fireEvent.click(button);
-  //assert
-  expect(screen.getByText(-1)).toBeInTheDocument();
-  expect(buttonClick).toBeTruthy();
-});
-
-test("go to about page", () => {
-  //arrange
-  render(<App />);
-  const link = screen.getByRole("link", { name: "About" });
-  //act
-  fireEvent.click(link);
-  //assert
-  expect(screen.getByText("on the contact page")).toBeInTheDocument();
-});
-
-test("go to about page, then back to home", () => {
-  //arrange
-  render(<App />);
-  const aboutLink = screen.getByRole("link", { name: "About" });
-  const homeLink = screen.getByRole("link", { name: "Home" });
-
-  fireEvent.click(aboutLink);
-  expect(screen.getByText("on the contact page")).toBeInTheDocument();
-
-  fireEvent.click(homeLink);
-  expect(screen.getByText("Enter an alephium address")).toBeInTheDocument();
 });
